be0b9347a4535579ab3f0eb7b1e26cc30e925286
amani387 <negaamanuel387@gmail.com>
2025-01-03 23:10:21 +0300

    adding script files

---
+++ task_1/scripts/data_combine.py
@@ -0,0 +1,10 @@
+for feature in features_with_na:
+    data = dataset.copy()
+    
+    # let's make a variable that indicates 1 if the observation was missing or zero otherwise
+    data[feature] = np.where(data[feature].isnull(), 1, 0)
+    
+    # let's calculate the mean SalePrice where the information is missing or present
+    data.groupby(feature)['SalePrice'].median().plot.bar()
+    plt.title(feature)
+    plt.show()
